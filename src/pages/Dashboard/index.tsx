dd7a2f833f0212ef3bcbb01f308a2067582afd19
Damian Justice <justice.damian96@gmail.com>
2025-04-17 06:09:43 +1000

    add oauth button for testing

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,23 @@
+import axios from "axios"
+import { config } from "../../services/config"
+import { Button } from "antd";
+
 export default function Dashboard() {
+    const requestOAuthLink = async () => {
+        var url = config.url.API_URL;
+        var token = localStorage.getItem("token");
+        if (token) {
+          axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+        }
+        await axios.get(`${url}/oauth`, { withCredentials: true })
+        .then(resp => {
+            window.location.href = resp.data
+        })
+    } 
     return(
         <>
         <h1>Under maintenance - coming soon!</h1>
+        <Button onClick={requestOAuthLink}> Begin OAuth</Button>
         </>
     )
 }
